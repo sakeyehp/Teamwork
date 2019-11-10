9c2156171da46c5ca30a3cb46e3e54deb774f033
sakeyehp <sakeyehp@yahoo.co.uk>
2019-11-10 23:56:00 +0000

    feature(gifs):add gifs enpoints

--- server.js
+++ server.js
@@ -1,4 +1,13 @@
-const express = require('express');
+import express from 'express';
+import dotenv from 'dotenv';
+// const bodyParser = require('body-parser');
+import 'babel-polyfill';
+import Gifs from './src/controllers/gifs';
+
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({extended:true}));
+
+dotenv.config();
 
 const app = express()
 
@@ -8,8 +17,15 @@ app.get('/', (req,res) => {
     return res.status(200).send({'message': 'Welcome to the TeamWork App'});
 })
 
+app.post('/api/v1/gifs', Gifs.create);
+app.get('/api/v1/gifs', Gifs.getAll);
+app.get('/api/v1/gifs/:id', Gifs.getOne);
+app.put('/api/v1/gifs/:id', Gifs.update);
+app.delete('/api/v1/gifs/:id', Gifs.delete);
+
 app.listen(3000)
 
 console.log('app running on port ', 3000);
 
 
+export default app;
